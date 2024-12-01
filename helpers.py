324b785341d672f38347713c5e51557213f2f11a
Alex Ross <adross1027@gmail.com>
2024-12-01 00:58:45 -0800

    function to make prediction

---
+++ helpers.py
@@ -0,0 +1,85 @@
+
+from dotenv import load_dotenv 
+import os
+from twikit import Client
+from user import User
+from datetime import datetime
+import cv2
+import numpy as np
+import urllib.request
+from fuzzywuzzy import fuzz
+
+# returns a Twikit client
+def get_client():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    envars = os.path.join(dir_path, '.env')
+    load_dotenv(envars)
+
+    client = Client('en-US')
+    client.load_cookies('cookies.json')
+    return client
+
+# returns the age in number of days
+def get_age(created_at: str):
+    date = datetime.strptime(created_at, '%a %b %d %H:%M:%S %z %Y')
+    now = datetime.now(date.tzinfo)
+    delta = now - date
+    age_in_days = delta.days
+    return age_in_days
+
+# returns 1 if the profile image is valid, 0 otherwise
+def analyze_profile_image(url: str):
+    image = load_image(url)
+    if image is None or not image.any(): return 0
+
+    gray_scale_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray_scale_img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    is_face_detected = len(faces) > 0
+
+    resolution = image.shape[:2] # (height, width)
+    is_high_quality = False if resolution[0] < 100 or resolution[1] < 100 else True
+
+    return 1 if is_face_detected or is_high_quality else 0
+    
+# loads an image from a URL
+def load_image(url: str):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    request = urllib.request.Request(url, headers=headers)
+    resp = urllib.request.urlopen(request)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
+# returns the number of identical tweet pairs and the total number of pairs
+def analyze_tweets_similarity(tweets: list, similarity_score_test_mark=95):
+    identical_tweet_pairs, num_pairs = 0, 0
+    tweets_analyzed = set()
+
+    for tweet in tweets:
+        if not tweet.text: continue # skip tweets without text
+        normalized_tweet = tweet.text.lower().strip()
+        # check for fuzzy matches
+        for other_tweet in tweets_analyzed:
+            num_pairs += 1
+            similarity_score = fuzz.ratio(normalized_tweet, other_tweet)
+            if similarity_score >= similarity_score_test_mark: 
+                identical_tweet_pairs += 1
+        tweets_analyzed.add(normalized_tweet)
+
+    return identical_tweet_pairs, num_pairs
+
+# returns the sentiment score of a text from -1 to 1
+def get_sentiment_score(text: str, sentiment_analyzer):
+
+    result = sentiment_analyzer(text)
+    label_scores = {res['label']: res['score'] for res in result}
+
+    # map the labels to weights: LABEL_0 -> -1, LABEL_1 -> 0, LABEL_2 -> +1
+    weights = {'LABEL_0': -1, 'LABEL_1': 0, 'LABEL_2': 1}
+
+    return sum(weights[label] * score for label, score in label_scores.items()) # score between -1 and 1 - negative, neutral, positive
+
+
+
---
+++ predict.py
@@ -0,0 +1,116 @@
+import asyncio
+from helpers import get_client, get_age, analyze_profile_image, get_sentiment_score, analyze_tweets_similarity
+from transformers import pipeline
+import torch
+import pandas as pd
+import joblib
+import shap
+
+TARGET_TWEETS = 125
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+# function that makes prediction
+# retuns prediction (human | bot | invalid), probability of prediction,
+# and top three user features that contributed to the prediction w/ their values
+# prediction can be invalid if the user is private (unable to access data) 
+# or has no tweets (not enough data)
+async def make_prediction(user_id: str):
+    client = get_client()
+
+    user = await client.get_user_by_id(user_id)
+    if not user: return {"prediction": "invalid", "probability": 0, "features": [], "error": "User not found."}
+
+    parsed_owned_tweets_count, parsed_owned_text_tweets_count, parsed_retweets_count, likes_count, replies_count, retweets_count, reply_tweets_count, urls_count, hashtags_count, quotes_tweet_count, sentiment =  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    age = get_age(user.created_at)
+
+    try:
+        # analyze profile picture with openCV
+        is_profile_image_valid = analyze_profile_image(user.profile_image_url)
+    except Exception as e:
+        print(f"Error analyzing profile image for user {user.id}: {e}")
+        return
+
+    tweets = []
+
+    finished = False
+    while not finished:
+        try:
+            res = await user.get_tweets('Tweets', count=TARGET_TWEETS)
+            while res and len(tweets) < TARGET_TWEETS:
+                tweets += res
+                res = await res.next()
+            finished = True
+        except Exception as e:
+            if 'Rate limit exceeded' in str(e): 
+                return {"prediction": "invalid", "probability": 0, "features": [], "error": "Server rate limit exceeded. Try again later."}
+    
+    if len(tweets) <= 0: 
+        return {"prediction": "invalid", "probability": 0, "features": [], "error": "User is either private or has no tweets."}
+
+    sentiment_analyzer = pipeline('sentiment-analysis', model='cardiffnlp/twitter-roberta-base-sentiment', device=device, max_length=512, truncation=True)
+    tweets = tweets[:TARGET_TWEETS]
+
+    for tweet in tweets:
+        if tweet.retweeted_tweet: 
+            parsed_retweets_count += 1
+            continue
+        if tweet.text:
+            sentiment += get_sentiment_score(tweet.text, sentiment_analyzer)
+            parsed_owned_text_tweets_count += 1
+        if tweet.in_reply_to: reply_tweets_count += 1
+        if tweet.is_quote_status: quotes_tweet_count += 1
+        parsed_owned_tweets_count += 1
+        hashtags_count += len(tweet.hashtags)
+        likes_count += tweet.favorite_count
+        replies_count += tweet.reply_count
+        retweets_count += tweet.retweet_count
+        urls_count += len(tweet.urls)
+
+    identical_tweet_pairs, num_tweet_pairs = analyze_tweets_similarity(tweets)
+
+    features = {
+        'account_age': age,
+        'is_blue_verified': user.is_blue_verified,
+        'profile_description_sentiment': round(get_sentiment_score(user.description, sentiment_analyzer), 3) if user.description else 0,
+        'following_count': user.following_count,
+        'followers_count': user.followers_count,
+        'following_to_followers': round(user.following_count / user.followers_count, 3) if user.followers_count > 0 else 0,
+        'is_possible_sensitive': 1 if user.possibly_sensitive else 0,
+        'is_default_profile_image': 1 if user.default_profile_image else 0,
+        'is_profile_banner': 1 if user.profile_banner_url else 0,
+        'is_profile_image_valid': 1 if is_profile_image_valid else 0,
+        'tweet_freq': round(parsed_owned_tweets_count / age, 3) if age > 0 else 0,
+        'likes_freq': round(likes_count / age, 3) if age > 0 else 0,
+        'media_freq': round(user.media_count / age, 3) if age > 0 else 0,
+        'followers_freq': round(user.followers_count / age, 3) if age > 0 else 0,
+        'following_freq': round(user.following_count / age, 3) if age > 0 else 0,
+        'replies_to_owned': round(reply_tweets_count / parsed_owned_tweets_count, 3) if parsed_owned_tweets_count > 0 else 0,
+        'quotes_to_owned': round(quotes_tweet_count / parsed_owned_tweets_count, 3) if parsed_owned_tweets_count > 0 else 0,
+        'retweets_to_owned': round(retweets_count / parsed_owned_tweets_count, 3) if parsed_owned_tweets_count > 0 else 0,
+        'avg_urls': round(urls_count / parsed_owned_tweets_count, 3) if parsed_owned_tweets_count > 0 else 0,
+        'avg_hashtags': round(hashtags_count / parsed_owned_tweets_count, 3) if parsed_owned_tweets_count > 0 else 0,
+        'identical_tweet_freq': round(identical_tweet_pairs / num_tweet_pairs, 3) if num_tweet_pairs > 0 else 0,
+        'avg_tweet_sentiment': round(sentiment / parsed_owned_text_tweets_count, 3) if parsed_owned_text_tweets_count > 0 else 0,
+        'avg_replies_per_follower': round(replies_count / parsed_owned_tweets_count / user.followers_count * 1000, 3) if user.followers_count > 0 and parsed_owned_tweets_count > 0 else 0,
+        'avg_likes_per_follower': round(likes_count / parsed_owned_tweets_count / user.followers_count * 1000, 3) if user.followers_count > 0 and parsed_owned_tweets_count > 0 else 0,
+        'avg_retweets_per_follower': round(retweets_count / parsed_owned_tweets_count / user.followers_count * 1000, 3) if user.followers_count > 0 and parsed_owned_tweets_count > 0 else 0,
+    }
+
+    user_df = pd.DataFrame([features])
+
+    model = joblib.load('model.joblib')
+    prediction = model.predict(user_df)
+    probabilities = model.predict_proba(user_df)
+
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(user_df)
+    feature_names = user_df.columns.tolist()
+
+    feature_contributions = list(zip(feature_names, shap_values[:, 0 if prediction[0] == 0 else 1]))
+    feature_contributions.sort(key=lambda x: abs(x[1]), reverse=True)
+    top_features = {feature: user_df[feature] for feature, _ in feature_contributions[:3]}
+
+    return {
+        'prediction': 'bot' if prediction[0] == 1 else 'human',
+        'probability': probabilities[0][1] if prediction[0] == 1 else probabilities[0][0],
+        'top_features': top_features
+    }
